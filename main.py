3e480776248486c41b6b1596da639f863c9dc8c6
Kirill Meshorer <volframhome@gmail.com>
2025-11-08 19:04:00 +0500

    create new app, add basic functional, made crud, made routers

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+from fastapi import FastAPI, Path
+from pydantic import BaseModel, EmailStr
+import uvicorn
+
+from items_views import router as items_router
+from users.views import router as users_router
+
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+@app.get("/")
+def hello_index():
+    return {
+        "message": "Hello, index!"
+    }
+
+@app.get("/hello/")
+def hello(name: str = "World"):
+    name = name.strip().title()
+    return {"message": f"Hello {name}"}
+
+
+#if __name__ == '__main__':
+ #   uvicorn.run("main:app", reload=True)
