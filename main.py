2366a6cfa09fc4849e0a2539b7d82384e04a6688
Kirill Meshorer <volframhome@gmail.com>
2025-11-09 15:30:21 +0500

    delete create_all for start app

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from items_views import router as items_router
-from core.models import Base, db_helper
 from users.views import router as users_router
 from api_v1 import router as router_v1
 from core.config import settings
@@ -9,8 +8,7 @@ from core.config import settings
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+
     yield
 
 
